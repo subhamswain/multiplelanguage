90d70ab6f0b5552bd05772511cc17afa995ebee0
subham swain <subham.swain@targetintegration.com>
2023-09-10 15:38:03 +0530

    multiplelanguage

---
+++ src/component/language-dropdown.js
@@ -0,0 +1,14 @@
+const Languageoption = (props) => {
+    return(
+        <div style={{marginTop:'50px'}}>
+            <select onChange={props.onChange}>
+                <option>Select Language</option>
+                <option value={'en'}>English</option>
+                <option value={'ko'}>Korean</option>
+                <option value={'chi'}>Chinese</option>
+                <option value={'hi'}>Hindi</option>
+            </select>
+        </div>
+    )
+}
+export default Languageoption;
